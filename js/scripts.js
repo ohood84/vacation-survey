5ab203480f7ad83fca2a31e0839ca63b2eb80f17
ohood84 <obeidat.ohood@gmail.com>
2019-02-17 21:13:13 -0800

    it will add fading images to the page and change the input box width tyle

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  // $("button").click(function(){
+   setTimeout($("#div1").fadeIn(3000),3000);
+  // $("#div2").fadeIn("slow");
+  // $("#div3").fadeIn(3000);
+// });
+});
