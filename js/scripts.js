07c0a570e8546c0bd48e47c14eb15ee92dcb1642
ohood84 <obeidat.ohood@gmail.com>
2019-02-20 11:53:05 -0800

    it will add three vacation suggestors

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   var x=1;
   while(x===1){}
    $("#div1").fadeToggle("slow");
+   $("#first-suggestor").fadetogglerIn("3000");
  }
   // $("#div2").fadeIn("slow");
   // $("#div3").fadeIn(3000);
